f23142d2497a49f23541e06db6e9c38573b3d230
everruler12 <erik.newhard@gmail.com>
2020-09-23 15:06:58 -0600

    new copy-title-and-url

--- public/bookmarklets/copy-title-and-url.js
+++ public/bookmarklets/copy-title-and-url.js
@@ -5,7 +5,7 @@
 
     function getTitle(url) {
         if (url.indexOf('mail.google.com') !== -1) {
-            return document.title.replace(/ - .*?@gmail\.com - Gmail$/, '')
+            return document.title.replace(/ - .*?@gmail\.com - Gmail$/, ' - Gmail')
         } else {
             return document.title
         }
@@ -13,39 +13,28 @@
 
     const url = getURL()
     const title = getTitle(url)
+
     copyLink(url, title)
 
     function copyLink(url, title) {
-        // append link
-        const containerId = "_hiddenCopyTextarea_"
-        var container = document.createElement('div')
-        container.style.position = "fixed"
-        container.style.left = "-9999px"
-        container.style.top = "0"
-        container.id = containerId
-        document.body.appendChild(container)
-        var target = document.createElement('a')
-        target.href = url
-        target.textContent = title
-        container.appendChild(target)
+        function listener(e) {
+            e.clipboardData.setData("text/plain", title + '\n' + url)
+            e.clipboardData.setData("text/html", `<a href="${url}">${title}</a>`)
+            e.preventDefault()
+        }
 
-        // get link data; https://stackoverflow.com/questions/53003980/how-to-copy-a-hypertext-link-into-clipboard-with-javascript-and-preserve-its-lin
-        const range = document.createRange()
-        range.selectNode(target)
-        const selection = window.getSelection()
-        selection.removeAllRanges()
-        selection.addRange(range)
+        document.addEventListener("copy", listener)
 
-        // copy
         try {
             document.execCommand("copy")
             console.log("Copied: \n", {
                 title,
                 url
             })
-            document.getElementById(containerId).remove() // shorten to target?
         } catch (e) {
-            console.log("Copy failed.")
+            console.log("Copy failed.", e)
+        } finally {
+            document.removeEventListener("copy", listener)
         }
     }
 })()
