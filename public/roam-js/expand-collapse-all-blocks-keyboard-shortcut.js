ee871fe6eba5ec388bc81b7a7dfa99d36266ae8d
everruler12 <erik.newhard@gmail.com>
2020-09-25 02:04:38 -0600

    Update expand-collapse-all-blocks-keyboard-shortcut.js

--- public/roam-js/expand-collapse-all-blocks-keyboard-shortcut.js
+++ public/roam-js/expand-collapse-all-blocks-keyboard-shortcut.js
@@ -1,35 +1,93 @@
-/*
-Keyboard shortcuts to expand/collapse all blocks on page.
-By Erik Newhard
-*/
+    ;
+    (function() {
+        waitForLoad({
+            name: 'jQuery',
+            load() {
+                appendFile("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js")
+            },
+            timeoutStep: 50,
+            timeoutLimit: 1000,
+            isLoaded() {
+                return window.jQuery !== undefined
+            },
+            callback() {
+                listenForKeydown()
+            }
+        })
 
-if (!window.jQuery) {
-    let s = document.createElement('script')
-    s.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
-    document.head.appendChild(s)
-    setTimeout(listenForKeydown, 500)
-} else {
-    listenForKeydown()
-}
+        function listenForKeydown() {
+            let listener = window.listeningForKeyboardShortcut_expandCollapseAllBlocks
 
-function listenForKeydown() {
-    if (window.listeningForKeydown) return
+            if (listener)
+                document.removeEventListener("keydown", listener)
 
-    document.addEventListener("keydown", function(e) {
-        // Ctrl/Cmd+Shift+Down to expand all blocks
-        if (e.keyCode == 40 && e.shiftKey && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-            e.preventDefault()
-            $('.page-title').find('button').click() // 3-dot menu next to page title
-            $('.bp3-portal').find('.bp3-menu').find('div:contains(Expand all)').click()
+            listener = function(e) {
+                // Ctrl/Cmd+Shift+Up to collapse all blocks
+                if ((e.ctrlKey || e.metaKey) && e.shiftKey && e.keyCode == 38) {
+                    e.preventDefault()
+                    $('.page-title').find('button').click() // 3-dot menu next to page title
+                    $('.bp3-portal').find('.bp3-menu').find('div:contains(Collapse all)').click()
+                }
+
+                // Ctrl/Cmd+Shift+Down to expand all blocks
+                if ((e.ctrlKey || e.metaKey) && e.shiftKey && e.keyCode == 40) {
+                    e.preventDefault()
+                    $('.page-title').find('button').click() // 3-dot menu next to page title
+                    $('.bp3-portal').find('.bp3-menu').find('div:contains(Expand all)').click()
+                }
+            }
+            console.log('listening')
+            document.addEventListener("keydown", listener)
+
+            window.listeningForKeyboardShortcut_expandCollapseAllBlocks = listener
         }
 
-        // Ctrl/Cmd+Shift+Up to collapse all blocks
-        if (e.keyCode == 38 && e.shiftKey && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-            e.preventDefault()
-            $('.page-title').find('button').click() // 3-dot menu next to page title
-            $('.bp3-portal').find('.bp3-menu').find('div:contains(Collapse all)').click()
+        function appendFile(url) {
+            let s = document.createElement('script')
+            s.src = url
+            document.head.appendChild(s)
         }
-    }, false)
 
-    window.listeningForKeydown = true
-}
+        function waitForLoad({
+            name,
+            load,
+            timeoutStep,
+            timeoutLimit,
+            isLoaded,
+            callback
+        }) {
+
+            if (isLoaded()) {
+                console.log(`${name}: Already loaded.`)
+                callback()
+                return
+            }
+
+            console.log(`${name}: Loading...`)
+            load()
+
+            if (isLoaded()) {
+                console.log(`${name}: Loaded synchronously without wait.`)
+                callback()
+                return
+            }
+
+            let counter = 0
+            wait()
+
+            function wait() {
+                if (counter * timeoutStep >= timeoutLimit) {
+                    console.log(`${name}: Timed-out after ${timeoutLimit} ms.`)
+                    alert(`Error: ${name} not loaded.`)
+                    // return
+                } else if (isLoaded()) {
+                    console.log(`${name}: Loaded after ${counter * timeoutStep} ms.`)
+                    callback()
+                    // return
+                } else {
+                    counter++
+                    setTimeout(wait, timeoutStep)
+                }
+            }
+        }
+    })();
