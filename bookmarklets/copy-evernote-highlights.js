d06329bd9180a16347a332f0aaa2d5a8a4c32696
everruler12 <erik.newhard@gmail.com>
2020-12-05 16:23:50 -0700

    update that works

--- bookmarklets/copy-evernote-highlights.js
+++ bookmarklets/copy-evernote-highlights.js
@@ -1,40 +1,43 @@
-const highlights = document.querySelectorAll('span[data-highlight]')
-copyHighlights(highlights)
+(function () {
+    const note_editor_document = document.querySelector('iframe[aria-label="Note Editor"]').contentWindow.document
+    const highlights = note_editor_document.querySelectorAll('span[data-highlight]')
+    copyHighlights(highlights)
 
-function copyHighlights(highlights) {
-    const text = hl_text(highlights)
-    const html = hl_html(highlights)
+    function copyHighlights(highlights) {
+        if (!highlights.length) {
+            alert('No Evernote highlights detected.')
+            return
+        }
 
-    if (text == "") {
-        alert('No Evernote highlights detected.')
-        return
+        const text = hl_text(highlights)
+        const html = hl_html(highlights)
+
+        function listener(e) {
+            e.clipboardData.setData("text/plain", text)
+            e.clipboardData.setData("text/html", html)
+            e.preventDefault()
+        }
+
+        document.addEventListener("copy", listener)
+
+        try {
+            document.execCommand("copy")
+            console.log("Copied: \n", {
+                text,
+                html
+            })
+        } catch (e) {
+            console.log("Copy failed.", e)
+        } finally {
+            document.removeEventListener("copy", listener)
+        }
     }
 
-    function listener(e) {
-        e.clipboardData.setData("text/plain", text)
-        e.clipboardData.setData("text/html", html)
-        e.preventDefault()
+    function hl_text(highlights) {
+        return Array.from(highlights).map(x => x.textContent).join('\r\n\r\n')
     }
 
-    document.addEventListener("copy", listener)
-
-    try {
-        document.execCommand("copy")
-        console.log("Copied: \n", {
-            text,
-            html
-        })
-    } catch (e) {
-        console.log("Copy failed.", e)
-    } finally {
-        document.removeEventListener("copy", listener)
+    function hl_html(highlights) {
+        return Array.from(highlights).map(x => x.innerHTML).join('<br>')
     }
-}
-
-function hl_text(highlights) {
-    return Array.from(highlights).map(x => x.textContent).join('\r\n\r\n')
-}
-
-function hl_html(highlights) {
-    return Array.from(highlights).map(x => x.innerHTML).join('<br>')
-}
+})()
