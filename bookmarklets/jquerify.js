533affdbbb24957f2a61dca035d36df197508fcc
everruler12 <erik.newhard@gmail.com>
2021-02-26 23:51:03 -0700

    updates

--- bookmarklets/jquerify.js
+++ bookmarklets/jquerify.js
@@ -1,28 +1,27 @@
+if (window.jQuery) {
+    console.log(`jQuerify: Already loaded`)
+    return
+}
 
-    if (window.jQuery) {
-        console.log(`jQuerify: Already loaded`)
+let s = document.createElement('script')
+s.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+document.head.appendChild(s)
+
+let counter = 0
+let timeoutLength = 5
+let timeoutLimit = 1000
+
+waitForLoad()
+
+function waitForLoad() {
+    if (counter * timeoutLength >= timeoutLimit) {
+        console.log(`jQuerify: Timed-out after ${timeoutLimit} ms`)
+        alert('Error: jQuery not loaded.')
         return
+    } else if (window.jQuery) {
+        console.log(`jQuerify: Loaded after ${counter * timeoutLength} ms`)
+    } else {
+        counter++
+        setTimeout(waitForLoad, timeoutLength)
     }
-
-    let s = document.createElement('script')
-    s.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
-    document.head.appendChild(s)
-
-    let counter = 0
-    let timeoutLength = 5
-    let timeoutLimit = 1000
-
-    waitForLoad()
-
-    function waitForLoad() {
-        if (counter * timeoutLength >= timeoutLimit) {
-            console.log(`jQuerify: Timed-out after ${timeoutLimit} ms`)
-            alert('Error: jQuery not loaded.')
-            return
-        } else if (window.jQuery) {
-            console.log(`jQuerify: Loaded after ${counter * timeoutLength} ms`)
-        } else {
-            counter++
-            setTimeout(waitForLoad, timeoutLength)
-        }
-    }
+}
