14c899e222679c0fd305ddc7c2a04dc89c2848fc
Erik Newhard <64292121+everruler12@users.noreply.github.com>
2022-10-28 19:45:07 -0600

    update bulk luma zoom

--- public/bookmarklets/bookmarklets/bulk-edit-luma-zoom-meetings.js
+++ public/bookmarklets/bookmarklets/bulk-edit-luma-zoom-meetings.js
@@ -1,12 +1,56 @@
+function jquerify() {
+    if (window.jQuery) {
+        console.log(`jQuerify: Already loaded`)
+        init()
+        return
+    }
+
+    let s = document.createElement('script')
+    s.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+    document.head.appendChild(s)
+
+    let counter = 0
+    let timeoutLength = 5
+    let timeoutLimit = 1000
+
+    waitForLoad()
+
+    function waitForLoad() {
+        if (counter * timeoutLength >= timeoutLimit) {
+            console.log(`jQuerify: Timed-out after ${timeoutLimit} ms`)
+            alert('Error: jQuery not loaded.')
+            return
+        } else if (window.jQuery) {
+            console.log(`jQuerify: Loaded after ${counter * timeoutLength} ms`)
+            init()
+        } else {
+            counter++
+            setTimeout(waitForLoad, timeoutLength)
+        }
+    }
+}
+
+
+
 init()
 function init() {
-    let new_zoomMeetingUrl = prompt("New Zoom Meeting URL")
-    new_zoomMeetingUrl = new_zoomMeetingUrl.trim()
 
-    if (!new_zoomMeetingUrl) return
+    // check that page is on lu.ma and ends it /edit
+    if (location.hostname != "lu.ma" || location.pathname.split('?')[0].split('/').pop() != "edit")
+        return
+
+
+    let new_zoomMeetingUrl = prompt("New Zoom Meeting URL")
+
+    if (!new_zoomMeetingUrl)
+        return
+
+    new_zoomMeetingUrl = new_zoomMeetingUrl.trim()
 
     let new_zoomMeetingPassword = prompt("New Zoom Meeting Password")
     new_zoomMeetingPassword = new_zoomMeetingPassword.trim()
+
+    var listOfFutureSessions = $('.session-list > .session-wrapper:not(:has(.passed))')
 
     listOfFutureSessions.toArray().map(el => {
         const zoomMeetingUrl = $(el).find('input')[0]
@@ -17,6 +61,7 @@ function init() {
     })
 
     function reactSet(el, newValue) {
+        // https://stackoverflow.com/a/62111884
         const valueSetter = Object.getOwnPropertyDescriptor(el, 'value').set
         const prototype = Object.getPrototypeOf(el)
         const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set
